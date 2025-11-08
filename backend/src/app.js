cc0a9c748b618da6df9fbde4f975f3f13d3094ba
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-11-08 14:51:58 +0530

    db connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser")
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("hello")
+})
+
+connectDB().then(() => {
+    console.log("db connected");
+    app.listen(process.env.PORT, () => {
+        console.log("server running on " + process.env.PORT);
+    })
+}).catch((err) => {
+    console.error(err.message);
+})
