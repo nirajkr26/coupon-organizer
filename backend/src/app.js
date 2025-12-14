40acda5e4a2de7e5652a1c7a5e9b7dc926c60134
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-12-14 17:42:04 +0530

    update configs for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors")
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://coupon-organizer.vercel.app",
     credentials: true
 }))
 app.use(express.json());
